2265401f1c6bed7d273413b7c55769b4cfc625ce
Laks0 <laksjoaquin@gmail.com>
2025-07-07 01:00:55 -0300

    intento fallido de gradiente

---
+++ TP3/src/descenso.py
@@ -0,0 +1,38 @@
+import numpy as np
+from metodos import metodo, W, generar_instancias
+
+# Gradiente de W
+def DW(x, puntos, pesos):
+    puntos_en_uso = puntos
+    pesos_en_uso = pesos
+    # Si es un punto, lo excluimos de la matriz de puntos para usar el diferencial
+    coincidencias = np.all(puntos == x, axis=1)
+    if np.any(coincidencias):
+        j = np.argmax(coincidencias)
+        puntos_en_uso = np.delete(puntos, j, axis=0)
+        pesos_en_uso = np.delete(pesos, j, axis=0)
+    norma = 1/np.linalg.norm(x - puntos_en_uso, axis=1)
+    return np.sum(np.diag(norma)@np.diag(pesos_en_uso)@(x-puntos_en_uso), axis=0)
+
+class Descenso(metodo):
+    def __init__(self, puntos, pesos, epsilon):
+        metodo.__init__(self, puntos, pesos)
+
+    def _iterar_desde_punto(self, punto_inicial):
+        x = punto_inicial
+        while True:
+            alpha = 1
+            D = DW(x, self._puntos, self._pesos)
+
+            if np.all(D < epsilon):
+                return x
+
+            while W(x - alpha * D, self._puntos, self._pesos) >= W(x, self._puntos, self._pesos) - .5 * alpha * D.T * D:
+                alpha = alpha / 2
+
+            x = x - alpha * D
+            print(D[0])
+
+if __name__ == "__main__":
+    puntos, pesos = generar_instancias()
+    print(DW(puntos[0]*10, puntos, pesos))
